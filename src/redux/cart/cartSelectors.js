9058e1ab59144a894f5f943e2d64ac47cfb3f2c2
der1x <130235333+Mark4353@users.noreply.github.com>
2026-04-26 18:35:06 +0300

    hw

--- src/redux/cart/cartSelectors.js
+++ src/redux/cart/cartSelectors.js
@@ -9,3 +9,4 @@ export const getCartCount = cartSelectors.selectTotal;
 export const getCartTotal = createSelector([getCartItems], cartItems =>
   cartItems.reduce((sum, item) => sum + (item.price || 0), 0),
 );
+
